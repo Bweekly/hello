860c4f1518a3558d4a736718ee88b2f83bf9c194
Braydon <bweekly2@huskers.unl.edu>
2021-08-26 17:00:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Braydon Weekly
+ * Date: 2021/26/08
+ *
+ * A simple hello world in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main (int argc, char **argv) {
+
+    printf("Braydon Weekly\n");
+    printf("Computer Engineering\n");
+
+    return 0;
+ }
